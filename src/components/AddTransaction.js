2c583a254eabdc190a2cd8355bf2a4cb470367a6
Aishwarya Naidu <aishwarya13naidu@gmail.com>
2021-03-01 22:58:14 +0530

    Dynamic data binding

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IncomeExpenses } from './IncomeExpenses'
 
 export const AddTransaction = () => {
+
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState(0)
+
     return (
         <>
             <h3>Add new transaction</h3>
             <form>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text ..." />
+                    <input type="text" value={ text } onChange={(event) => setText(event.target.value)} placeholder="Enter text ..." />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">
                         Amount <br />
-                        { `negative - IncomeExpenses, positive - income` }
+                        {`negative - IncomeExpenses, positive - income`}
                     </label>
-                    <input type="number" placeholder="Enter amount ..." />
+                    <input type="number" value={ amount } onChange={(event) => setAmount(event.target.value)} placeholder="Enter amount ..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
