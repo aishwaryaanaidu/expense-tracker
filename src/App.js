9e94745da38b55e9414dd503d04cd910f0e361c5
Aishwarya Naidu <aishwarya13naidu@gmail.com>
2021-03-01 22:57:35 +0530

    Context API settings

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { Balance } from './components/Balance'
 import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList'
 import { AddTransaction } from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses /> 
-        <TransactionList />
-        <AddTransaction />
-      </div>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
     </div>
   );
 }
