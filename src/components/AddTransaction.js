0dabb68aef9413cf77c78c1d78706ccf26bef66d
Aishwarya Naidu <aishwarya13naidu@gmail.com>
2021-03-05 21:17:12 +0530

    Add and delete transaction flow

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { IncomeExpenses } from './IncomeExpenses'
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
 
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
+    const { addTransaction } = useContext(GlobalContext)
 
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction)
+    }
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={ text } onChange={(event) => setText(event.target.value)} placeholder="Enter text ..." />
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,12 @@
-import React from 'react' 
+import React, { useContext } from 'react' 
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+'
+    const { deleteTransaction } = useContext(GlobalContext)
 
     return (
-        <li className={transaction.amount < 0 ? "minus" : "plus"}>{ transaction.text } <span>{ sign }{ Math.abs(transaction.amount) }</span><button className="delete-bin">x</button></li>
+        <li className={transaction.amount < 0 ? "minus" : "plus"}>{ transaction.text } <span>{ sign }{ Math.abs(transaction.amount) }</span>
+        <button className="delete-bin" onClick={() => deleteTransaction(transaction.id)} >x</button></li>
     )
 }
